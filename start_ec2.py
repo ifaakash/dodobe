9c4a05a1ed29c4360b2b2e3eb7347680c02e8c79
Dodo <work.dodoclub@gmail.com>
2025-03-30 09:45:28 +0000

    change runner for backend deployment

--- start_ec2.py
+++ start_ec2.py
@@ -44,7 +44,7 @@ def start_runner(instance_id):
 
 def main():
     # start the runner
-    start_runner('i-024617e6eb0a267f7') # New Runner Instance ID
+    start_runner('i-007ff4cc767a25bae') # New Runner Instance ID
 
     # wait for 5 min
     #print("Waiting for 5min....")
