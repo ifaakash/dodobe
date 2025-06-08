95839aa8c6b915dacb9086a9ac73044b5b4b94b0
SarthakSri98 <sarthaksri1998@gmail.com>
2025-06-08 16:34:38 +0530

    feat/del-dist

--- dist/types/invoice.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InvoiceStatus = void 0;
-var InvoiceStatus;
-(function (InvoiceStatus) {
-    InvoiceStatus["PAID"] = "paid";
-    InvoiceStatus["UNPAID"] = "unPaid";
-})(InvoiceStatus || (exports.InvoiceStatus = InvoiceStatus = {}));
